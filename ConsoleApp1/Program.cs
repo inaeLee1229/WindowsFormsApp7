7cdd366967a659fa94c73d761482ade4382cca16
iiina <iiina@DESKTOP-A4R70HK>
2022-12-10 20:30:56 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class GenericApp<T>
+    {
+        public string Connect(int OP, T a, T b)
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int OP;
+            int int_indata_1, int_indata_2;
+            double double_indata_1, double_indata_2;
+            Console.Write("Read OP from key ? :");
+            OP = int.Parse(Console.ReadLine());
+            if(OP == 0)
+            {
+                string Text = "double_indata_1.ToString()" + "double_indta_2.ToString()";
+                Console.WriteLine(Text);
+               
+            }
+            if(OP==1)
+            {
+                string Text = "int_indata_1.ToString()" + "int_indta_2.ToString()";
+                Console.WriteLine(Text);
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null && listBox2.SelectedItem!=null)
+            {
+                int num1 =Convert.ToInt32( listBox1.SelectedItem);
+                int num2 = Convert.ToInt32( listBox2.SelectedItem);
+                int result = num1 + num2;
+                textBox1.Text = result.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
